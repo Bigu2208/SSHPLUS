87b52cba6f7610f6a0cdd74fa1e072bae2db9bd1
Bigu2208 <111613950+Bigu2208@users.noreply.github.com>
2022-08-19 21:59:12 -0300

    Add files via upload

---
+++ atlasconfig.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+
+crontab -r >/dev/null 2>&1
+(
+	crontab -l 2>/dev/null
+	echo "@daily /bin/verifatt"
+	echo "@reboot /etc/autostart"
+	echo "* * * * * /etc/autostart"
+	echo "0 */6 * * * /bin/uexpired"
+	echo "* * * * * /sbin/iptables -F"
+	echo "* * * * * /bin/portas"
+	echo "* * * * * /root/backrest.sh"
+) | crontab -
+
+echo -e "\033[1;32mINSTALANDO O BADVPN !\033[0m\n"
+	    
+	        cd $HOME
+            wget https://www.dropbox.com/s/tgkxdwb03r7w59r/badvpn-udpgw -o /dev/null
+            mv -f $HOME/badvpn-udpgw /bin/badvpn-udpgw
+            chmod 777 /bin/badvpn-udpgw
+	   
+	   echo -e "\n\033[1;32mINICIANDO O BADVPN... \033[0m\n"
+           screen -dmS udpvpn /bin/badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000 --max-connections-for-client 10
+           [[ $(grep -wc "udpvpn" /etc/autostart) = '0' ]] && {
+		       echo -e "ps x | grep 'udpvpn' | grep -v 'grep' || screen -dmS udpvpn /bin/badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 10000 --max-connections-for-client 10 --client-socket-sndbuf 10000" >> /etc/autostart
+		   } || {
+		       sed -i '/udpvpn/d' /etc/autostart
+		       echo -e "ps x | grep 'udpvpn' | grep -v 'grep' || screen -dmS udpvpn /bin/badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 10000 --max-connections-for-client 10 --client-socket-sndbuf 10000" >> /etc/autostart
+		   }
+           sleep 1
+
+
+
+             cd /bin/ && wget https://cdn.discordapp.com/attachments/968040569769181194/1010344869975306331/portas
+             chmod 777 portas
+             
+             cd /root/ && wget https://raw.githubusercontent.com/Bigu2208/SSHPLUS/main/backrest.sh
+             chmod 777 backrest.sh
+            
+
+					sleep 1
+					screen -dmS ws python /etc/SSHPlus/wsproxy.py 8009
+					[[ $(grep -wc "wsproxy.py" /etc/autostart) = '0' ]] && {
+						echo -e "netstat -tlpn | grep -w 8009 > /dev/null || {  screen -r -S 'ws' -X quit;  screen -dmS ws python /etc/SSHPlus/wsproxy.py 8009; }" >>/etc/autostart
+					} || {
+						sed -i '/wsproxy.py/d' /etc/autostart
+						echo -e "netstat -tlpn | grep -w 8009 > /dev/null || {  screen -r -S 'ws' -X quit;  screen -dmS ws python /etc/SSHPlus/wsproxy.py 8009; }" >>/etc/autostart
+					}
+				echo ""
+				echo -e "\033[1;32mINICIANDO O WEBSOCKET\033[1;33m"
+				echo ""
+
+    sleep 10
+
+
+            
+
