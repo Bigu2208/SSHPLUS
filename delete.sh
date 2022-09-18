e1bf2589485270fb6960c514fcc46e57ff07ad17
Bigu2208 <111613950+Bigu2208@users.noreply.github.com>
2022-09-18 10:16:00 -0300

    Delete delete.sh

--- delete.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-shopt -s extglob
-rm  !(AtlantusMakeAccount.sh|AtlantusKillUser.sh|AtlantusRemoveAccount.sh|usuarios.db|AlterarSenha.sh|AtlantusMakeAccountForce.sh|delete.sh|maketest.sh)
