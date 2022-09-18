a5c70e141bf8dc18001dcd5cd952f47afa6dc002
Bigu2208 <111613950+Bigu2208@users.noreply.github.com>
2022-09-18 11:06:27 -0300

    Create crontab.sh

---
+++ crontab.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+crontab -r >/dev/null 2>&1
+(
+	crontab -l 2>/dev/null
+	echo "@daily /bin/verifatt"
+	echo "@reboot /etc/autostart"
+	echo "* * * * * /etc/autostart"
+	echo "0 */6 * * * /bin/uexpired"
+	echo "* * * * * /sbin/iptables -F"
+	echo "* * * * * /bin/portas"
+	echo "0 */1 * * * /root/deletar.sh"
+) | crontab -
