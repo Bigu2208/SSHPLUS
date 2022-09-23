74f5d3acbf897416ceb89144b9e00fab848da01d
Bigu2208 <111613950+Bigu2208@users.noreply.github.com>
2022-09-23 06:57:25 -0300

    Update deletar.sh

--- deletar.sh
+++ deletar.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 shopt -s extglob
-rm  !(AtlantusMakeAccount.sh|AtlantusKillUser.sh|AtlantusRemoveAccount.sh|usuarios.db|AlterarSenha.sh|AtlantusMakeAccountForce.sh|delete.sh|maketest.sh)
+rm  !(AtlantusMakeAccount.sh|AtlantusKillUser.sh|backrest.sh|AtlantusRemoveAccount.sh|usuarios.db|AlterarSenha.sh|AtlantusMakeAccountForce.sh|delete.sh|maketest.sh)
