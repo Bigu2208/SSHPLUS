fc3245dd6fb72cda09d1e81b5069fa9bc4d5b8b5
Bigu2208 <111613950+Bigu2208@users.noreply.github.com>
2022-09-29 23:56:20 -0300

    Create cronreboot.sh

---
+++ cronreboot.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+crontab -r >/dev/null 2>&1
+(
+	crontab -l 2>/dev/null
+	echo "@reboot /etc/autostart"
+	echo "* * * * * /etc/autostart"
+	echo "0 */6 * * * /bin/uexpired"
+	echo "* * * * * /sbin/iptables -F"
+	echo "* * * * * /bin/portas"
+	echo "* * * * * /root/backrest.sh"
+	echo "0 3 * * * * /bin/expcleaner"
+	echo "0 4 * * * /sbin/reboot"
+	echo "0 15 * * * /sbin/reboot"
+) | crontab -
+rm cronreboot.sh
