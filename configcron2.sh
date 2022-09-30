587b861d07918d9664bec93bfee4cd50c384eb4d
Bigu2208 <111613950+Bigu2208@users.noreply.github.com>
2022-09-29 23:37:56 -0300

    Update and rename configcron.sh to configcron2.sh

--- configcron2.sh
+++ configcron2.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-
-wget https://raw.githubusercontent.com/Bigu2208/SSHPLUS/main/deletar.sh
-chmod 777 deletar.sh
 crontab -r >/dev/null 2>&1
 (
 	crontab -l 2>/dev/null
-	echo "@daily /bin/verifatt"
 	echo "@reboot /etc/autostart"
 	echo "* * * * * /etc/autostart"
 	echo "0 */6 * * * /bin/uexpired"
 	echo "* * * * * /sbin/iptables -F"
 	echo "* * * * * /bin/portas"
-	echo "0 */6 * * * /root/deletar.sh"
-) | crontab -
+	echo "* * * * * /root/backrest.sh"
+	echo "0 3 * * * * /bin/expcleaner"
+	echo "0 4 * * * * /sbin/reboot"
+	echo "0 15 * * * * /sbin/reboot"
+	) | crontab -
