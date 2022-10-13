51bc82801dc4d3c2cdd287a05f79f9c6bda86e46
Bigu2208 <111613950+Bigu2208@users.noreply.github.com>
2022-10-13 16:00:28 -0300

    Update and rename cronnnf.sh to cronnnff.sh

--- cronnnff.sh
+++ cronnnff.sh
@@ -3,12 +3,12 @@ crontab -r >/dev/null 2>&1
 (
 	crontab -l 2>/dev/null
 	echo "@reboot /etc/autostart"
-  echo "* * * * * * /etc/autostart"
+        echo "* * * * * /etc/autostart"
 	echo "0 */6 * * * /bin/uexpired"
 	echo "@reboot /sbin/iptables -F"
 	echo "@reboot /bin/portas"
 	echo "* * * * * /root/backrest.sh"
-  echo "* * * * * /root/deletarr.sh"
+        echo "* * * * * /root/deletarr.sh"
 	echo "0 4 * * * /sbin/reboot"
 	echo "0 15 * * * /sbin/reboot"
 ) | crontab -
