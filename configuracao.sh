458a3aa24ef08ed0d8f68ec64ca6218cdf6f2109
Bigu2208 <111613950+Bigu2208@users.noreply.github.com>
2022-11-10 11:57:46 -0300

    Update configuracao.sh

--- configuracao.sh
+++ configuracao.sh
@@ -3,16 +3,16 @@ crontab -r >/dev/null 2>&1
 (
 	crontab -l 2>/dev/null
 	echo "@reboot /etc/autostart"
-  echo "* * * * * /etc/autostart"
+        echo "* * * * * /etc/autostart"
 	echo "0 */6 * * * /bin/uexpired"
 	echo "@reboot /sbin/iptables -F"
 	echo "@reboot /bin/portas"
 	echo "* * * * * /root/backrest.sh"
   echo "*/6 * * * * /root/deletar2.sh"
   echo "*/5 * * * * /root/limpeza.sh"
-	echo "0 4 * * * /sbin/reboot"
+	echo "0 3 * * * /sbin/reboot"
+	echo "0 22 * * * /sbin/reboot"
 	echo "0 15 * * * /sbin/reboot"
-	echo "@reboot /root/abrir.sh"
 ) | crontab -
 sleep2
 	   wget https://raw.githubusercontent.com/Bigu2208/SSHPLUS/main/deletar2.sh
@@ -20,9 +20,6 @@ sleep2
 	   wget https://raw.githubusercontent.com/Bigu2208/SSHPLUS/main/limpeza.sh
            sleep 2
            chmod 777 limpeza.sh
-	   sleep 1
-	   wget https://raw.githubusercontent.com/Bigu2208/SSHPLUS/main/abrir.sh
-	   chmod 777 abrir.sh
 	   sleep 1
 	   echo -e "\n\033[1;32mINICIANDO O BADVPN... \033[0m\n"
            screen -dmS udpvpn /bin/badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000 --max-connections-for-client 10
@@ -41,6 +38,9 @@ sleep2
              
              cd /root/ && wget https://raw.githubusercontent.com/Bigu2208/SSHPLUS/main/backrest.sh
              chmod 777 backrest.sh
+	     
+	     /bin/portas
+	     
             
 
 					sleep 1
